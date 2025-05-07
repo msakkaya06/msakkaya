044c71ee76c2b2e19e82cb4dfdc1bae4fd79144f
msakkaya06 <msakkaya06@gmail.com>
2025-05-07 21:39:27 +0300

    refact

--- MSCloud/auth/services/user_service.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-
-class UserService:
-    @staticmethod
-    def create_user(username: str, email: str, password: str) -> User:
-        return User.objects.create_user(username=username, email=email, password=password)
